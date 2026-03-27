34784a70c4fa5da0f886465391be77769cbf2806
SiGMan <sigman@ioupg.com>
2026-03-27 03:15:27 -0700

    chore: root workspace scaffold with tooling config

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+  env: { node: true, es2022: true },
+  ignorePatterns: ['node_modules/', 'dist/'],
+}
